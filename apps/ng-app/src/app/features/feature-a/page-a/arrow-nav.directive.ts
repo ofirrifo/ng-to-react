977ea6f254c54c3fe587124f5fcbcc0dc5b9dd1b
Jorge Barnaby <jorge.barnaby@sentinelone.com>
2023-08-10 21:33:38 -0400

    Minor refactor arrow-nav.directive

--- apps/ng-app/src/app/features/feature-a/page-a/arrow-nav.directive.ts
+++ apps/ng-app/src/app/features/feature-a/page-a/arrow-nav.directive.ts
@@ -14,43 +14,47 @@ export class ArrowNavDirective {
   @Output() itemSelected: EventEmitter<string> = new EventEmitter<string>();
 
   private readonly ACTIVE_ITEM_CSS_CLASS = 'active';
+  private readonly ACTIVE_ITEM_SELECTOR = `.${this.ACTIVE_ITEM_CSS_CLASS}`;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('document:keydown.Enter', ['$event'])
-  onEnter(event: KeyboardEvent): void {
+  onEnter(event: KeyboardEvent) {
     event.preventDefault();
-    this.itemSelected.emit(this.el.nativeElement.querySelector('.active').innerText);
-    const activeElement = this.el.nativeElement.querySelector('.active');
+
+    const activeElement = this.el.nativeElement.querySelector(this.ACTIVE_ITEM_SELECTOR);
     if (activeElement) {
+      this.itemSelected.emit(activeElement.innerText);
       this.renderer.removeClass(activeElement, this.ACTIVE_ITEM_CSS_CLASS);
     }
+  }
+
+  private onKeyArrow(
+    event: KeyboardEvent,
+    nextSelector: (element: HTMLElement) => ChildNode | null
+  ) {
+    event.preventDefault();
+
+    const activeElement = this.el.nativeElement.querySelector(this.ACTIVE_ITEM_SELECTOR);
+    const selectedElement = this.el.nativeElement.querySelector('.selected');
+    let elementToActive = this.el.nativeElement.childNodes[0];
+    if (activeElement) {
+      this.renderer.removeClass(activeElement, this.ACTIVE_ITEM_CSS_CLASS);
+      elementToActive = nextSelector(activeElement);
+    } else if (selectedElement) {
+      elementToActive = nextSelector(selectedElement);
+    }
+    this.renderer.addClass(elementToActive, this.ACTIVE_ITEM_CSS_CLASS);
+    elementToActive.focus();
   }
 
   @HostListener('document:keydown.ArrowDown', ['$event'])
-  onKeyArrowDown(event: KeyboardEvent): void {
-    event.preventDefault();
-
-    const activeElement = this.el.nativeElement.querySelector('.active');
-    let elementToActive = this.el.nativeElement.childNodes[0];
-    if (activeElement) {
-      this.renderer.removeClass(activeElement, this.ACTIVE_ITEM_CSS_CLASS);
-      elementToActive = activeElement.nextSibling;
-    }
-    this.renderer.addClass(elementToActive, this.ACTIVE_ITEM_CSS_CLASS);
-    elementToActive.focus();
+  onKeyArrowDown(event: KeyboardEvent) {
+    this.onKeyArrow(event, (element) => element.nextSibling);
   }
 
   @HostListener('document:keydown.ArrowUp', ['$event'])
-  onKeyArrowUp(event: KeyboardEvent): void {
-    event.preventDefault();
-    const activeElement = this.el.nativeElement.querySelector('.active');
-    let elementToActive = this.el.nativeElement.childNodes[0];
-    if (activeElement) {
-      this.renderer.removeClass(activeElement, this.ACTIVE_ITEM_CSS_CLASS);
-      elementToActive = activeElement.previousSibling;
-    }
-    this.renderer.addClass(elementToActive, this.ACTIVE_ITEM_CSS_CLASS);
-    elementToActive.focus();
+  onKeyArrowUp(event: KeyboardEvent) {
+    this.onKeyArrow(event, (element) => element.previousSibling);
   }
 }
